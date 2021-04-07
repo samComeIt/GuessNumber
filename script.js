904e14594c52b97d0822cfb567979ff8c3eb8d91
samComeIt <sanhae1996@gmail.com>
2021-04-07 11:19:26 +0900

    handling click event

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ document.querySelector('.score').textContent = 10;
 
 console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
+
+/* click event */
+document.querySelector('.check').addEventListener('click', function () {
+  //console.log(document.querySelector('.guess').value);
+  //document.querySelector('.message').textContent = 'Correct Number :D';
+
+  //const guess = document.querySelector('.guess').value;
+  const guess = Number(document.querySelector('.guess').value);
+
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    // 0 - false, 1 - true
+    document.querySelector('.message').textContent = 'No number!';
+  }
+});
