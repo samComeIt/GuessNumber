cbafe9615089302285f3a365e352b27dd1b39658
samComeIt <sanhae1996@gmail.com>
2021-04-07 10:54:29 +0900

    selecting and manipulating elemnts

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use script';
+
+console.log(document.querySelector('.message').textContent);
+
+/*
+DOM - DOcument Object Model
+Structured representation of HTML Documents allows javascript to access HTML elements and styles to manipulate(change text, HTML, attributes, and even CSS styles) them.
+*/
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
