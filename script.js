042c8eba9c11d447dc12a0935f579eb3d6e2afff
samComeIt <sanhae1996@gmail.com>
2021-04-07 20:44:58 +0900

    click event for again btn

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ DOM - DOcument Object Model
 Structured representation of HTML Documents allows javascript to access HTML elements and styles to manipulate(change text, HTML, attributes, and even CSS styles) them.
 */
 
-document.querySelector('.number').textContent = 13;
+//document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
 console.log(document.querySelector('.guess').value);
@@ -18,7 +18,19 @@ const guessNumber = Math.trunc(Math.random() * 20) + 1; //Math.random();
 
 let score = 20;
 
-document.querySelector('.number').textContent = guessNumber;
+/* click event for again btn*/
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  const guessNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = ''; // empty string
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
 
 /* click event */
 document.querySelector('.check').addEventListener('click', function () {
@@ -36,8 +48,9 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'No number!';
   } else if (guess === guessNumber) {
     document.querySelector('.message').textContent = 'Correct number!';
-    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').textContent = guessNumber;
 
+    document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
     // when guess it too high
